58edb362ddeec53886c6c5d5b7cba5e7e1fd7386
Marcela Melara <marcela.melara@intel.com>
2026-05-23 15:40:03 -0500

    Prepare v0.3.0 release
    Signed-off-by: Marcela Melara <marcela.melara@intel.com>

--- src/manifest/common.rs
+++ src/manifest/common.rs
@@ -24,7 +24,7 @@ use tdx_workload_attestation::get_platform_name;
 use time::OffsetDateTime;
 use uuid::Uuid;
 
-const CLAIM_GENERATOR: &str = "atlas-cli:0.2.0";
+const CLAIM_GENERATOR: &str = "atlas-cli:0.3.0";
 
 /// Asset type enum to distinguish between models, datasets, software, and evaluations
 pub enum AssetKind {
@@ -1079,7 +1079,7 @@ mod tests {
         let config = make_test_manifest_config();
         let claim = generate_c2pa_claim(&config, AssetKind::Model).unwrap();
         assert!(claim.instance_id.starts_with("urn:c2pa:"));
-        assert_eq!(claim.claim_generator_info, "atlas-cli:0.2.0");
+        assert_eq!(claim.claim_generator_info, "atlas-cli:0.3.0");
     }
 
     // #[test]
